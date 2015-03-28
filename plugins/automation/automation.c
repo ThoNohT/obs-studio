f9981039eb034321004d8c02af54faa273cdaf55
Eric Bataille <e.c.p.bataille@gmail.com>
2015-03-28 21:37:11 +0100

    automation: Added a processing thread.
    Linked to pthreads and created a thread that currently just loops and does nothing.

--- plugins/automation/automation.c
+++ plugins/automation/automation.c
@@ -1,4 +1,5 @@
 #include <obs-module.h>
+#include <util/threading.h>
 
 #define blog(log_level, format, ...) \
 	blog(log_level, "[automation] " format, ##__VA_ARGS__)
@@ -16,6 +17,9 @@ OBS_MODULE_USE_DEFAULT_LOCALE("automation", "en-US")
 
 OBS_MODULE_AUTHOR("ThoNohT")
 
+pthread_t processing_thread;
+bool stop;
+
 const char *obs_module_name()
 {
 	return "OBS Automation";
@@ -26,9 +30,27 @@ const char *obs_module_description()
 	return "Provides several automation options for obs-studio.";
 }
 
+void process(void)
+{
+	while (!stop) {
+		info("Processing.");
+
+		_sleep(1000);
+	}
+
+	info("Processing complete.");
+}
+
 bool obs_module_load(void)
 {
 	info("Starting plugin.");
+
+	stop = false;
+	if (pthread_create(&processing_thread, NULL, process, NULL))
+	{
+		warn("Failed to create the processing thread.");
+		return false;
+	}
 
 	return true;
 }
@@ -36,6 +58,12 @@ bool obs_module_load(void)
 bool obs_module_unload(void)
 {
 	info("Stopping plugin.");
+	
+	stop = true;
+	if(pthread_join(processing_thread, NULL)) {
+		warn("Failed to stop the processing thread.");
+		return false;
+	}
 
 	return true;
 }
