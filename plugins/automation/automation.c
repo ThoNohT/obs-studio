d1e8498ab49185cbaa4e009dbf2b3392643c4327
Eric Bataille <e.c.p.bataille@gmail.com>
2015-03-28 15:27:41 +0100

    automation: Added extra module methods.
    Implemented some extra methods defined in obs-module.h

--- plugins/automation/automation.c
+++ plugins/automation/automation.c
@@ -1,11 +1,41 @@
 #include <obs-module.h>
 
+#define blog(log_level, format, ...) \
+	blog(log_level, "[automation] " format, ##__VA_ARGS__)
+
+#define debug(format, ...) \
+	blog(LOG_DEBUG, format, ##__VA_ARGS__)
+#define info(format, ...) \
+	blog(LOG_INFO, format, ##__VA_ARGS__)
+#define warn(format, ...) \
+	blog(LOG_WARNING, format, ##__VA_ARGS__)
+
 OBS_DECLARE_MODULE()
 
 OBS_MODULE_USE_DEFAULT_LOCALE("automation", "en-US")
 
+OBS_MODULE_AUTHOR("ThoNohT")
+
+const char *obs_module_name()
+{
+	return "OBS Automation";
+}
+
+const char *obs_module_description()
+{
+	return "Provides several automation options for obs-studio.";
+}
+
 bool obs_module_load(void)
 {
+	info("Starting plugin.");
+
 	return true;
 }
 
+bool obs_module_unload(void)
+{
+	info("Stopping plugin.");
+
+	return true;
+}
